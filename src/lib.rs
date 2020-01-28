d5b09872d5a011fb514e9b19f5c78c954cd1d89f
Sebastian K <s3bk@protonmail.com>
2020-01-28 12:57:46 +0300

    initial

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#[macro_use] extern crate log2;
+pub mod view;
+
+#[cfg(target_os="linux")]
+pub mod gl;
+
+#[cfg(target_arch="wasm32")]
+pub mod webgl;
+
+pub use view::{show, Interactive, State};
